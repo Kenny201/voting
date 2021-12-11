b065b45ee1408446811513e38ec874f276331ce7
Vitaiy Izmestjev <panins899@gmail.com>
2021-12-11 17:36:32 +0300

    add models Idea

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <livewire:styles>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -131,5 +132,6 @@
 
     </div>
 </main>
+<livewire:scripts>
 </body>
 </html>
